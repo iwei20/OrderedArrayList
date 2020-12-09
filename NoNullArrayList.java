90cd64a1458c6973121c374cc58b2f102da3b39b
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-12-09 12:38:35 -0500

    Implement constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
+
+    public NoNullArrayList() {}
+    public NoNullArrayList(int initialCapacity) {
+        super(initialCapacity);
+    }
+
     public T set(int index, T element) {
         if(element == null) throw new IllegalArgumentException("Null is an illegal argument");
         return super.set(index, element);
