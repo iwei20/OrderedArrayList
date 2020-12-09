cc0c8c176d268498c794a2591c2865275c5fff83
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-12-09 13:09:19 -0500

    Implement both add()

---
+++ OrderedArrayList.java
@@ -0,0 +1,30 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+    public OrderedArrayList() {}
+    public OrderedArrayList(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    // Does not guarantee stability
+    public boolean add(T element) {
+        int low = 0;
+        int high = size() - 1;
+        int index = -1;
+        while(low < high && index == -1) {
+            int mid = low + (high - low) / 2;
+            if(element.compareTo(get(mid)) == 0) {
+                index = mid;
+            } else if(element.compareTo(get(mid)) == 1){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if(index == -1) index = low;
+        super.add(index, element);
+        return true;
+    }
+
+    public void add(int index, T element) {
+        add(element);
+    }
+}
