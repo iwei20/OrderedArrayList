88801539bbb655b17541f18167dd7daf9abd7a50
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-12-10 21:28:04 -0500

    Add tests for OrderedArrayList

--- Tester.java
+++ Tester.java
@@ -14,5 +14,37 @@ public class Tester {
         for(Integer i : nlist) {
             System.out.print(i + " ");
         }
+        System.out.println();
+
+        OrderedArrayList<Integer> olist = new OrderedArrayList<Integer>();
+        olist.add(50);
+        olist.add(90);
+        olist.add(120);
+        olist.add(5434, 127);
+        try {
+            olist.set(2, null);
+        } catch(IllegalArgumentException e) {
+            System.out.println("Skipped null");
+            e.printStackTrace();
+        }
+
+        olist.set(2, 12);
+
+        try {
+            olist.add(3, null);
+        } catch(IllegalArgumentException e) {
+            System.out.println("Skipped null");
+            e.printStackTrace();
+        }
+
+        olist.add(3, 91);
+        olist.add(2, 12);
+        olist.add(90);
+        olist.add(8, 120);
+        for(Integer i : olist) {
+            System.out.print(i + " ");
+        }
+
+        
     }
 }
